f64d332bbf4817b340e369d22b508728b6602b45
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-18 12:56:13 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>counter : {{count}}</h1>
+    <button @click="inc(1)">Increment</button>
+    <button @click="dec(1)">Decrement</button>
+    <Child :count="count" :incfn="inc" :decfn="dec"></Child>
+  </div>
+</template>
+
+<script>
+import Child from './child1';
+export default {
+  components: {
+    Child : Child
+  },
+  data:function(){
+    return{
+      count : 0
+    }
+  },
+  methods:{
+    inc(x){
+      this.count += x;
+    },
+    dec(x){
+      this.count -= x;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/child1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>child counter: {{count}}</h3>
+    <button @click="inc1">Increment</button>
+    <button @click="dec1">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["count","decfn","incfn"],
+  methods:{
+      inc1(){
+          this.incfn(1)
+        //   this.$emit("incit",1);
+      },
+      dec1(){
+          this.decfn(1)
+        //   this.$emit("decit",1);
+      }
+  }
+}
+</script>
+
+<style scoped>
+    div{
+        text-align: center;
+    }
+</style>
